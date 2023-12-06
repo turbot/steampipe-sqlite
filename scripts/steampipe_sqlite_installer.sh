3c34442db1335b71231fb02b623c66e4eca7d105
Puskar Basu <puskar@turbot.com>
2023-12-06 11:06:40 +0530

    Modify and rename steampipe_sqlite_installer

--- scripts/steampipe_sqlite_installer.sh
+++ scripts/steampipe_sqlite_installer.sh
@@ -7,8 +7,8 @@ main() {
   BOLD=$(tput bold)
   NORMAL=$(tput sgr0)
 
-  if ! command -v gunzip >/dev/null; then
-    echo "Error: 'gunzip' is required." 1>&2
+  if ! command -v tar >/dev/null; then
+    echo "Error: 'tar' is required." 1>&2
     exit 1
   fi
 
@@ -17,10 +17,10 @@ main() {
     exit 1
   else
     case $(uname -sm) in
-    "Darwin x86_64") target="darwin_amd64.so.gz" ;;
-    "Darwin arm64") target="darwin_arm64.so.gz" ;;
-    "Linux x86_64") target="linux_amd64.so.gz" ;;
-    "Linux aarch64") target="linux_arm64.so.gz" ;;
+    "Darwin x86_64") target="darwin_amd64.tar.gz" ;;
+    "Darwin arm64") target="darwin_arm64.tar.gz" ;;
+    "Linux x86_64") target="linux_amd64.tar.gz" ;;
+    "Linux aarch64") target="linux_arm64.tar.gz" ;;
     *) echo "Error: '$(uname -sm)' is not supported yet." 1>&2;exit 1 ;;
     esac
   fi
@@ -31,10 +31,10 @@ main() {
   # Generate the URI for the FDW
   if [ "$version" = "latest" ]; then
     uri="https://api.github.com/repos/turbotio/steampipe-plugin-${plugin}/releases/latest"
-    asset_name="steampipe_sqlite_${plugin}_${target}"
+    asset_name="steampipe_sqlite_${plugin}.${target}"
   else
     uri="https://api.github.com/repos/turbotio/steampipe-plugin-${plugin}/releases/tags/${version}"
-    asset_name="steampipe_sqlite_${plugin}_${target}"
+    asset_name="steampipe_sqlite_${plugin}.${target}"
   fi
 
   # Read the GitHub Personal Access Token
@@ -59,11 +59,7 @@ main() {
      -o "$asset_name" -L --create-dirs
 
   # Use gunzip to extract it
-  gunzip $asset_name
-
-  # get the filename without the .gz extension and rename it to remove the OS and architecture
-  filename=$(echo $asset_name | sed 's/\.gz$//')
-  mv $filename steampipe_sqlite_${plugin}.so
+  tar -xvf $asset_name
 
   # move the .so file to the desired location if provided
   if [ "$location" != "$(pwd)" ]; then
@@ -78,32 +74,27 @@ main() {
 }
 
 validate_inputs() {
-  # Default values
-  plugin=""
-  version="latest"  # default version
-  location=$(pwd)   # default location to current working directory
+  # Check if plugin is provided as an argument
+  if [ $# -eq 0 ] || [ -z "$1" ]; then
+    read -p "Enter the plugin name: " plugin
+  else
+    plugin=$1
+  fi
 
-  # Regex for version (assuming version starts with 'v' followed by semantic versioning, e.g., v1.0.0, v2.1.3)
-  version_regex='^v[0-9]+(\.[0-9]+)*$'
+  # Check if version is provided as an argument
+  if [ $# -lt 2 ] || [ -z "$2" ]; then
+    read -p "Enter version (default: latest): " version
+    version=${version:-latest}  # Default to 'latest' if input is empty
+  else
+    version=$2
+  fi
 
-  # Check the number of arguments
-  if [ $# -eq 0 ]; then
-      echo "Usage: $0 <plugin> [version] [location]"
-      exit 1
-  elif [ $# -eq 1 ]; then
-      plugin=$1
-  elif [ $# -eq 2 ]; then
-      if [[ $2 =~ $version_regex ]]; then
-          plugin=$1
-          version=$2
-      else
-          plugin=$1
-          location=$2
-      fi
-  elif [ $# -eq 3 ]; then
-      plugin=$1
-      version=$2
-      location=$3
+  # Check if location is provided as an argument
+  if [ $# -lt 3 ] || [ -z "$3" ]; then
+    read -p "Enter location (default: current directory): " location
+    location=${location:-$(pwd)}  # Default to current directory if input is empty
+  else
+    location=$3
   fi
 }
 
